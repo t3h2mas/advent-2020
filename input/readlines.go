21fba790293d9a33a42fa09d9c5272b0af3c6d5d
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-02 08:38:06 -0700

    add input.Readlines method returning []string

--- input/readlines.go
+++ input/readlines.go
@@ -29,3 +29,22 @@ func ReadIntLines(fname string) ([]int, error) {
 
 	return lines, scanner.Err()
 }
+
+func ReadLines(fname string) ([]string, error) {
+	file, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}

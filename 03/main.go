b8f249c6c81281647b549419246b7225119fa8cd
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-03 08:59:31 -0700

    move grid / slope code out of main

--- 03/main.go
+++ 03/main.go
@@ -1,60 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/t3h2mas/advent-2020/03/slopes"
+
+	"github.com/t3h2mas/advent-2020/03/grid"
 
 	"github.com/t3h2mas/advent-2020/input"
 )
 
-type Grid []string
-
-var Tree = '#'
-var Open rune = '.'
-
-var OutOfBoundsError = errors.New("OutOfBounds")
-
-func (g Grid) CellAt(x, y int) (rune, error) {
-	var cell rune
-	if y < 0 || y > len(g)-1 {
-		return cell, OutOfBoundsError
-	}
-
-	cell = rune(g[y][x%len(g[y])])
-
-	return cell, nil
-}
-
 func main() {
-	var slope Grid
+	var slope grid.ScrollingGrid
 	slope, err := input.ReadLines("./input")
 	if err != nil {
 		panic(err)
 	}
 
-	rightPace := 3
-	downPace := 1
-
-	x := 0
-	y := 0
-
-	treeCount := 0
-
-	for {
-		cell, err := slope.CellAt(x, y)
-
-		if err != nil {
-			break
-		}
-
-		if cell == Tree {
-			treeCount++
-		}
-
-		x += rightPace
-		y += downPace
-	}
-
 	fmt.Printf("Day three, part one")
+
+	direction := slopes.NewTobogganDirection(3, 1)
+
+	encounters := slopes.PathEncounters(slope, direction)
+
+	treeCount := encounters['#']
 	fmt.Printf("solution: %d\n", treeCount)
+
+	fmt.Println("Day three, part two")
+
 }

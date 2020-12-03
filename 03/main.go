5703cb08028c7fba5ce37c2454eff8ced588e7e6
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-03 09:19:27 -0700

    day three part two

--- 03/main.go
+++ 03/main.go
@@ -17,15 +17,30 @@ func main() {
 		panic(err)
 	}
 
+	tree := '#'
+
 	fmt.Printf("Day three, part one")
 
 	direction := slopes.NewTobogganDirection(3, 1)
 
 	encounters := slopes.PathEncounters(slope, direction)
 
-	treeCount := encounters['#']
+	treeCount := encounters[tree]
 	fmt.Printf("solution: %d\n", treeCount)
 
 	fmt.Println("Day three, part two")
+	directions := []*slopes.TobogganDirection{
+		slopes.NewTobogganDirection(1, 1),
+		slopes.NewTobogganDirection(3, 1),
+		slopes.NewTobogganDirection(5, 1),
+		slopes.NewTobogganDirection(7, 1),
+		slopes.NewTobogganDirection(1, 2),
+	}
 
+	treeEncounters := 1
+	for _, d := range directions {
+		encounters := slopes.PathEncounters(slope, d)
+		treeEncounters = treeEncounters * encounters[tree]
+	}
+	fmt.Printf("solution: %d\n", treeEncounters)
 }

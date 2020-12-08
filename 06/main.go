02a7656dd6c0220277a9328c5b0178b341183aac
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-08 09:59:56 -0700

    day six part two

--- 06/main.go
+++ 06/main.go
@@ -23,6 +23,7 @@ func main() {
 	formGroups := splitEntries(declarationFormAnswers)
 
 	fmt.Println("Day six, part one")
+	// For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
 	answeredYes := 0
 	for _, group := range formGroups {
 		answersByPerson := strings.Split(group, "\n")
@@ -33,4 +34,42 @@ func main() {
 	}
 
 	fmt.Printf("Solution: %d\n", answeredYes)
+
+	// For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+	fmt.Println("Day six, part two")
+	allAnsweredYes := 0
+	for _, group := range formGroups {
+		fmt.Printf("Starting group '%s'\n", strings.TrimSpace(group))
+		answersByPerson := strings.Split(strings.TrimSpace(group), "\n")
+		fmt.Printf("%+v\n", answersByPerson)
+
+		var groupAnswerIntersection customs.FormAnswers
+
+		for _, answers := range answersByPerson {
+			formAnswers := customs.ParseIndividualAnswers(answers)
+			fmt.Printf("From: '%s'\n", answers)
+			fmt.Printf("Processing answers... %+v\n", formAnswers)
+
+			if groupAnswerIntersection == nil {
+				groupAnswerIntersection = formAnswers
+				fmt.Printf("groupAnswerIntersection initialized: %+v\n", groupAnswerIntersection)
+				continue
+			}
+
+			// apply intersection of current form answers to group answers
+			for question := range groupAnswerIntersection {
+				if !formAnswers.Has(question) {
+					fmt.Printf("Removing '%c' from group\n", question)
+					// an individual did not answer yes to question, remove from group
+					delete(groupAnswerIntersection, question)
+				}
+			}
+		}
+
+		fmt.Printf("len of groupAnswerIntersection: %d\n", len(groupAnswerIntersection))
+
+		allAnsweredYes += len(groupAnswerIntersection)
+	}
+
+	fmt.Printf("Solution: %d\n", allAnsweredYes)
 }

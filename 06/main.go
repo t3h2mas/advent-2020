715597e4d7ba1eadae127b0efce97f40cf260b82
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-08 10:02:50 -0700

    remove print debugs (dont @ me)

--- 06/main.go
+++ 06/main.go
@@ -39,34 +39,26 @@ func main() {
 	fmt.Println("Day six, part two")
 	allAnsweredYes := 0
 	for _, group := range formGroups {
-		fmt.Printf("Starting group '%s'\n", strings.TrimSpace(group))
 		answersByPerson := strings.Split(strings.TrimSpace(group), "\n")
-		fmt.Printf("%+v\n", answersByPerson)
 
 		var groupAnswerIntersection customs.FormAnswers
 
 		for _, answers := range answersByPerson {
 			formAnswers := customs.ParseIndividualAnswers(answers)
-			fmt.Printf("From: '%s'\n", answers)
-			fmt.Printf("Processing answers... %+v\n", formAnswers)
 
 			if groupAnswerIntersection == nil {
 				groupAnswerIntersection = formAnswers
-				fmt.Printf("groupAnswerIntersection initialized: %+v\n", groupAnswerIntersection)
 				continue
 			}
 
 			// apply intersection of current form answers to group answers
 			for question := range groupAnswerIntersection {
 				if !formAnswers.Has(question) {
-					fmt.Printf("Removing '%c' from group\n", question)
 					// an individual did not answer yes to question, remove from group
 					delete(groupAnswerIntersection, question)
 				}
 			}
 		}
-
-		fmt.Printf("len of groupAnswerIntersection: %d\n", len(groupAnswerIntersection))
 
 		allAnsweredYes += len(groupAnswerIntersection)
 	}

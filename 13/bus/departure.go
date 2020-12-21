9a63be673e48493fc2cb3ba5d71f65d3447a5c28
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-21 09:18:12 -0700

    remove "faster mod" implementation

--- 13/bus/departure.go
+++ 13/bus/departure.go
@@ -1,7 +1,5 @@
 package bus
 
-import "math/bits"
-
 type Departure struct {
 	busID int
 	time  int
@@ -42,57 +40,6 @@ func ClosestDeparture(buses []Bus, targetDeparture int) Departure {
 		busID,
 		closestDeparture,
 	}
-}
-
-type Int64 struct {
-	absd   uint64
-	hi, lo uint64
-	neg    bool
-}
-
-// NewInt64 initializes a new pre-computed inverse.
-func NewInt64(d int64) Int64 {
-	var neg bool
-	if d < 0 {
-		neg = true
-		d = -d
-	}
-	absd := uint64(d)
-
-	hi, r := ^uint64(0)/absd, ^uint64(0)%absd
-	lo, _ := bits.Div64(r, ^uint64(0), absd)
-
-	var c uint64 = 1
-	if absd&(absd-1) == 0 {
-		c++
-	}
-	lo, c = bits.Add64(lo, c, 0)
-	hi, _ = bits.Add64(hi, 0, c)
-	return Int64{
-		absd: absd,
-		hi:   hi,
-		lo:   lo,
-		neg:  neg,
-	}
-}
-
-func (d Int64) Mod(n int64) int64 {
-	var neg bool
-	if n < 0 {
-		n = -n
-		neg = true
-	}
-	hi, lo := bits.Mul64(d.lo, uint64(n))
-	hi += d.hi * uint64(n)
-	modlo1, _ := bits.Mul64(lo, d.absd)
-	mod, modlo2 := bits.Mul64(hi, d.absd)
-	var c uint64
-	_, c = bits.Add64(modlo1, modlo2, 0)
-	mod, _ = bits.Add64(mod, 0, c)
-	if neg {
-		return -int64(mod)
-	}
-	return int64(mod)
 }
 
 func EarliestOrderedDeparture(buses map[int]Bus) int {

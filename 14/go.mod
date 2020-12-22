7147773bba1b35ef20ee481af2df4d152c3c10b8
Thomas Noe <thomas.noe@madwiremedia.com>
2020-12-21 17:29:04 -0700

    day fourteen, part one

---
+++ 14/go.mod
@@ -0,0 +1,5 @@
+module github.com/t3h2mas/advent-2020/14
+
+go 1.14
+
+require github.com/t3h2mas/advent-2020/input v0.0.0-20201221163754-ac1e796a9ee6 // indirect
---
+++ 14/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/t3h2mas/advent-2020/14/docking"
+
+	"github.com/t3h2mas/advent-2020/input"
+)
+
+// "mask = 01X0000010101001X1000000X0X000110101"
+func maskFor(s string) string {
+	parts := strings.Split(s, " ")
+
+	return parts[2]
+}
+
+type writeCmd struct {
+	address int
+	value   int
+}
+
+// mem[57918] = 102433847
+func writeCmdFor(s string) writeCmd {
+	re := regexp.MustCompile(`mem\[(\d+)\] = (\d+)`)
+	parts := re.FindStringSubmatch(s)
+
+	address, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic(err)
+	}
+
+	value, err := strconv.Atoi(parts[2])
+	if err != nil {
+		panic(err)
+	}
+
+	return writeCmd{
+		address: address,
+		value:   value,
+	}
+}
+
+func main() {
+	commands, err := input.ReadLines("./input")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Day fourteen, part one")
+
+	program := docking.NewProgram()
+	var mask string
+	for _, instruction := range commands {
+		if strings.HasPrefix(instruction, "mask") {
+			mask = maskFor(instruction)
+			continue
+		}
+
+		cmd := writeCmdFor(instruction)
+
+		program.Write(cmd.address, cmd.value, mask)
+	}
+
+	sum := 0
+	for _, val := range program.Memory() {
+		sum += val
+	}
+
+	fmt.Printf("solution: %d\n", sum)
+}
